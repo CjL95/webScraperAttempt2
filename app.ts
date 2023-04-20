f0de4a2c274e741478eb10bf99b35e0701d8b958
CjL95 <71044991+CjL95@users.noreply.github.com>
2023-04-20 16:28:35 -0400

    scrapers properly write text to file

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
-const site = 'https://successcenters.org/';
-import { urls } from './utils/urls';
 import { scrapeManager } from './scrapers/scrape-manager';
 import { scrapeHeaderFooter } from './scrapers/header-footer-scraper';
 import { scrapeBody } from './scrapers/body-scraper';
 
-scrapeManager(scrapeHeaderFooter, scrapeBody);
+try{
+    scrapeManager(scrapeHeaderFooter, scrapeBody)
+}catch(e){
+    console.log(e);
+};
